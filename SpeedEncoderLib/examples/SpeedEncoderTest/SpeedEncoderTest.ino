9a6fc8378d15f44e3d516e5c4b7fb9fdae6ad7e5
BruceDaOne <53716929+BruceDaOne@users.noreply.github.com>
2021-10-06 11:46:39 +1100

    Update SpeedEncoderTest.ino

--- SpeedEncoderLib/examples/SpeedEncoderTest/SpeedEncoderTest.ino
+++ SpeedEncoderLib/examples/SpeedEncoderTest/SpeedEncoderTest.ino
@@ -2,12 +2,12 @@
 
 SpeedEncoder motor1(2,3); //instantiate class
 
-void setup(){
+void setup() {
     Serial.begin(115200);
 }
 
-void loop(){
-    if(motor1.Update()){ //will return true everytime the desired timestep has passed
+void loop() {
+    if(motor1.Update()) { //will return true everytime the desired timestep has passed
         Serial.println(motor1.GetSpeed()); //print speed
     }
 
